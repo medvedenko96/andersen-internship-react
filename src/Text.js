73c14ddd1292b2933007325aba5bb6b1558e47c1
Medvedenko Yaroslav <medvedenko96@gmail.com>
2018-07-16 13:03:06 +0300

    task_1 is done

---
+++ src/Text.js
@@ -0,0 +1,1 @@
+import React, { Component } from 'react';const divStyle = {  color: 'blue',  fontSize: '20px',}export default class Text extends Component {  render() {    return React.createElement('div', { style: divStyle }, this.props.someText);  }}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import React from 'react';import { render } from 'react-dom';render(<h1>hh</h1>, document.getElementById('root'));
+import React from 'react';import { render } from 'react-dom';import Text from './Text';render(  React.createElement(    "div",    null,    React.createElement(Text, { someText: 'text' }),    React.createElement("img", { src: "http://www.jsweet.org/wp-content/uploads/2016/04/react-logo-300x289.png", alt: "react" })  ), document.getElementById('root'));
