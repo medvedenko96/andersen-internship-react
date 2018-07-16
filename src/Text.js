8b1876fe380fcc7bbc0796aff846fc4c21a4526c
Medvedenko Yaroslav <medvedenko96@gmail.com>
2018-07-16 13:10:14 +0300

    fix

--- src/Text.js
+++ src/Text.js
@@ -1,1 +1,12 @@
-import React, { Component } from 'react';const divStyle = {  color: 'blue',  fontSize: '20px',}export default class Text extends Component {  render() {    return React.createElement('div', { style: divStyle }, this.props.someText);  }}
+import React, { Component } from 'react';
+
+const divStyle = {
+  color: 'blue',
+  fontSize: '20px',
+}
+
+export default class Text extends Component {
+  render() {
+    return React.createElement('div', {style: divStyle}, this.props.someText);
+  }
+}
