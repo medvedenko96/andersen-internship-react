bd73b0779cec79ef7b6fd9b449734b19bf765264
Medvedenko Yaroslav <medvedenko96@gmail.com>
2018-07-16 14:23:29 +0300

    fix

--- src/Text.js
+++ src/Text.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const divStyle = {
   color: 'blue',
   fontSize: '20px',
 }
 
-export default class Text extends Component {
-  render() {
-    return React.createElement('div', {style: divStyle}, this.props.someText);
-  }
+export default function Text (props){
+    return React.createElement('div', { style: divStyle }, props.someText); 
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ render(
   React.createElement(
     "div",
     null,
-    React.createElement(Text, {someText: 'text'}),
+    React.createElement(Text, { someText: 'text' }),
     React.createElement("img", { src: "http://www.jsweet.org/wp-content/uploads/2016/04/react-logo-300x289.png", alt: "react" })
   ), document.getElementById('root')
 );
