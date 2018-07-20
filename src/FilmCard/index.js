a2882ee3e0788d669a049b9af5c4d62c7e99b6b3
Medvedenko Yaroslav <medvedenko96@gmail.com>
2018-07-20 11:19:21 +0300

    test

--- src/FilmCard/index.js
+++ src/FilmCard/index.js
@@ -1,1 +1,26 @@
-import React, { Component }  from 'react';import { NavLink } from 'react-router-dom'import styles from './index.css';export default class FilmCard extends Component {  constructor (props) {    super(props)    this.state = {      films : []    }  }  render() {    return (      <NavLink className={styles.FilmCard}  to={`/film/${this.props.id}`}>        <img src={this.props.img} alt={this.props.name}/>        <div>          <div className='nameFilm'>            {this.props.name}          </div>          <div className='ScoreFilm'>            {this.props.score}          </div>        </div>      </NavLink>    )  }}
+import React, { Component }  from 'react';
+import { NavLink } from 'react-router-dom'
+
+import styles from './index.css';
+
+export default class FilmCard extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <NavLink className={styles.FilmCard}  to={`/film/${this.props.id}`}>
+        <img src={this.props.img} alt={this.props.name}/>
+        <div>
+          <div className='nameFilm'>
+            {this.props.name}
+          </div>
+          <div className='ScoreFilm'>
+            {this.props.score}
+          </div>
+        </div>
+      </NavLink>
+    )
+  }
+}
